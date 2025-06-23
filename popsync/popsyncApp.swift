eaf565be964e8b9a40354ce9f10d65984f81d94a
noodles <elvis.perlika@studio.unibo.it>
2025-06-23 15:01:52 +0200

    Initial Commit

---
+++ popsync/popsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  popsyncApp.swift
+//  popsync
+//
+//  Created by Elvis Perlika on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct popsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
