fb3c308aa4d269529d6fee8cf0294f55468425d1
noodles <elvis.perlika@studio.unibo.it>
2025-06-23 18:59:13 +0200

    feat: add button to go to next page

--- popsync/ContentView.swift
+++ popsync/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("welcome on pop sync")
+                .font(.title)
+
+            HStack {
+                Spacer()
+                Button(action: {
+                    // go to next page
+                }) {
+                    Image(systemName: "arrow.right")
+                        .symbolRenderingMode(.hierarchical)
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
+                        .frame(width: 60, height: 60)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                        .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 5)
+                }
+                Spacer()
+            }
         }
         .padding()
     }
